dc0a01a2244c03bd17b0062a5654bc7d12e183a8
Rodhis <rodrigonoliva@gmail.com>
2023-11-29 16:58:43 -0300

    partial button script logic

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 "use strict";
+// window.onload = () => {
+//     const radioForm:HTMLFormElement = document.getElementById('form-page3') as HTMLFormElement
+//     radioForm.addEventListener('submit', validateForm)
+//     // console.log(radioForm)
+//     }
 function changeButtonText(text) {
     const buttonText = document.querySelector('.next-button');
     buttonText.innerHTML = text;
 }
-function validateForm() {
-    const radioForm = document.querySelector('.radio3');
-    if (radioForm) {
-        radioForm.onsubmit;
-    }
-    else {
-        alert('Please select your financial market');
-    }
+function skipButton() {
+    const skipButton = document.getElementById('skip-button');
+    skipButton.addEventListener('click', () => window.location.replace("/src/pages/page4.html"));
 }
+// function validateForm(event:any):void {
+//     event.preventDefault()
+//     console.log('Executou')
+//     const radioForm:HTMLFormElement = document.getElementById('form-page3') as HTMLFormElement
+//     const select:HTMLSelectElement = document.getElementById('selectPage3') as HTMLSelectElement
+//     const selectedValue = select.value 
+//     if (selectedValue) console.log(selectedValue) 
+//         else
+//             console.log('Não funcionou')
+//     }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
+// window.onload = () => {
+    
+//     const radioForm:HTMLFormElement = document.getElementById('form-page3') as HTMLFormElement
+
+//     radioForm.addEventListener('submit', validateForm)
+//     // console.log(radioForm)
+//     }
+
 function changeButtonText(text: string) {
     const buttonText:HTMLElement = document.querySelector('.next-button') as HTMLElement
     buttonText.innerHTML = text
-}
-
-function validateForm():void {
-    const radioForm:HTMLTemplateElement = document.querySelector('.radio3')! as HTMLTemplateElement
-
-    if (radioForm) {
-        radioForm.onsubmit
-    } else {
-        alert('Please select your financial market')
-    }
-
 
 }
+
+function skipButton():void {
+    const skipButton:HTMLButtonElement = document.getElementById('skip-button') as HTMLButtonElement
+
+    skipButton.addEventListener('click', () => window.location.replace("/src/pages/page4.html") )
+}
+
+// function validateForm(event:any):void {
+//     event.preventDefault()
+//     console.log('Executou')
+//     const radioForm:HTMLFormElement = document.getElementById('form-page3') as HTMLFormElement
+            
+//     const select:HTMLSelectElement = document.getElementById('selectPage3') as HTMLSelectElement
+//     const selectedValue = select.value 
+    
+//     if (selectedValue) console.log(selectedValue) 
+//         else
+//             console.log('Não funcionou')
+//     }
