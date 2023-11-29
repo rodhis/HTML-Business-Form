542630361432b60db2e347fd1d4530539fc2e28b
Rodhis <rodrigonoliva@gmail.com>
2023-11-29 15:01:49 -0300

    page 4

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ function changeButtonText(text) {
     const buttonText = document.querySelector('.next-button');
     buttonText.innerHTML = text;
 }
+function validateForm() {
+    const radioForm = document.querySelector('.radio3');
+    if (radioForm) {
+        radioForm.onsubmit;
+    }
+    else {
+        alert('Please select your financial market');
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,15 @@ function changeButtonText(text: string) {
     const buttonText:HTMLElement = document.querySelector('.next-button') as HTMLElement
     buttonText.innerHTML = text
 }
+
+function validateForm():void {
+    const radioForm:HTMLTemplateElement = document.querySelector('.radio3')! as HTMLTemplateElement
+
+    if (radioForm) {
+        radioForm.onsubmit
+    } else {
+        alert('Please select your financial market')
+    }
+
+
+}
